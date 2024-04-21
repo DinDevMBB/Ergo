6b40b81b1068915cfb90207c81caf34365640d53
DinDevMBB <62540599+DinDevMBB@users.noreply.github.com>
2024-04-21 08:24:30 +0530

    Update requirements.txt

--- Ergo.py
+++ Ergo.py
@@ -332,7 +332,12 @@ elif app_mode =='Run on Image':
 
 
 ###########################################################################################
+    def clear_cache():
+        keys = list(st.session_state.keys())
+        for key in keys:
+            st.session_state.pop(key)
 
+    st.button('Clear Cache', on_click=clear_cache)
 
 
 
