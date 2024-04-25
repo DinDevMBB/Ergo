6368d467979b3c47c64339b47f37ac4122ff4b85
Dinesh <dindev@gmail.com>
2024-04-25 17:29:22 +0530

    landmarks updated

--- media.py
+++ media.py
@@ -127,63 +127,10 @@ def detectPose(image_pose,MIN_CONFIEDENCE):
 
         # Right AnkLe.
         r_ankle_x = int(lm.landmark[lmPose.RIGHT_ANKLE].x * w)
-        r_ankle_y = int(lm.landmark[lmPose.RIGHT_ANKLE].y * h)             
-        
-        # Calculate angles.
-        neck_inclination =0
-        neck_inclination = findAngle(l_shldr_x, l_shldr_y, l_ear_x, l_ear_y)
-
-        # calcualte neck tilt
-        neck_tilt =0
-        left_dist = findDistance(l_ear_x, l_ear_y, l_shldr_x, l_shldr_y)
-        right_dist =findDistance(r_ear_x, r_ear_y, r_shldr_x, r_shldr_y)
-        neck_tilt =left_dist/right_dist
-        
-        # calcuate trunk angle
-        left_bend =0
-        left_bend = 180- findAngle(l_shldr_x, l_shldr_y, l_hip_x, l_hip_y)
-
-        # calcualte trunk twist
-        trunk_twist =0
-        left_dist = findDistance(r_shldr_x, r_shldr_y, l_hip_x, l_hip_y)
-        right_dist =findDistance(l_shldr_x, l_shldr_y, r_hip_x, r_hip_y)
-        trunk_twist =left_dist/right_dist
-
-        # Calcualte knee angle
-        a = np.array([l_hip_x,l_hip_y])
-        b = np.array([l_knee_x,l_knee_y])
-        c = np.array([l_ankle_x,l_ankle_y])
-        left_leg_angle =find3pointangle(a,b,c)
-
-        a = np.array([r_hip_x,r_hip_y])
-        b = np.array([r_knee_x,r_knee_y])
-        c = np.array([r_ankle_x,r_ankle_y])
-        right_leg_angle =find3pointangle(a,b,c)
-
-        leg_angle = max(left_leg_angle,right_leg_angle)
-        leg_ratio = left_leg_angle/right_leg_angle
+        r_ankle_y = int(lm.landmark[lmPose.RIGHT_ANKLE].y * h)  
 
 
-        # # mark dot
-        #x1 =int(left_wrist_landmark[0])
-        #y1 =int(left_wrist_landmark[1])
-        # cv2.circle(landmarked_image,(x1,y1), 7, yellow, -1)
 
-        # Upper Arm Angle
-        rigth_uarm_bend =0
-        rigth_uarm_bend = 180- findAngle(r_shldr_x, r_shldr_y, r_wrist_x, r_wrist_y)
-    
-        # lower Arm Angle
-        rigth_larm_bend =0
-        rigth_larm_bend = 180- findAngle(r_elbow_x, r_elbow_y, r_wrist_x, r_wrist_y)
-
-        # Wrist Angle
-        wrist_bend_angle =0
-        wrist_bend_angle =180- findAngle(r_index_x, r_index_y, r_wrist_x, r_wrist_y)
-
-        # Nose
-        nose_x = int(lm.landmark[lmPose.NOSE].x * w)
-        nose_y = int(lm.landmark[lmPose.NOSE].y * h) 
 
         # mid of shoulder
         m_shldr_x =int((r_shldr_x+l_shldr_x)/2)
@@ -217,20 +164,80 @@ def detectPose(image_pose,MIN_CONFIEDENCE):
         cv2.line(landmarked_image, (r_wrist_x,r_wrist_y), (r_elbow_x,r_elbow_y), blue, 4)
         cv2.line(landmarked_image, (l_shldr_x,l_shldr_y), (l_elbow_x,l_elbow_y), blue, 4)
         cv2.line(landmarked_image, (l_wrist_x,l_wrist_y), (l_elbow_x,l_elbow_y), blue, 4)
-        #cv2.circle(landmarked_image,(x1,y1), 7, yellow, -1)
+        #cv2.circle(landmarked_image,(x1,y1), 7, yellow, -1)           
+        
+        # Calculate angles.
+        neck_inclination =0
+        #neck_inclination = findAngle(l_shldr_x, l_shldr_y, l_ear_x, l_ear_y)
+        neck_inclination = findAngle(m_shldr_x, m_shldr_y, m_ear_x, m_ear_y)
+
+        # calcualte neck tilt
+        neck_tilt =0
+        left_dist = findDistance(l_ear_x, l_ear_y, l_shldr_x, l_shldr_y)
+        right_dist =findDistance(r_ear_x, r_ear_y, r_shldr_x, r_shldr_y)
+        neck_tilt =left_dist/right_dist
+        
+        # calcuate trunk angle
+        bend_angle =0
+        #left_bend = 180- findAngle(l_shldr_x, l_shldr_y, l_hip_x, l_hip_y)
+        bend_angle = 180- findAngle(m_shldr_x, m_shldr_y, m_hip_x, m_hip_y)
+
+        # calcualte trunk twist
+        trunk_twist =0
+        left_dist = findDistance(r_shldr_x, r_shldr_y, l_hip_x, l_hip_y)
+        right_dist =findDistance(l_shldr_x, l_shldr_y, r_hip_x, r_hip_y)
+        trunk_twist =left_dist/right_dist
+
+        # Calcualte knee angle
+        a = np.array([l_hip_x,l_hip_y])
+        b = np.array([l_knee_x,l_knee_y])
+        c = np.array([l_ankle_x,l_ankle_y])
+        left_leg_angle =find3pointangle(a,b,c)
+
+        a = np.array([r_hip_x,r_hip_y])
+        b = np.array([r_knee_x,r_knee_y])
+        c = np.array([r_ankle_x,r_ankle_y])
+        right_leg_angle =find3pointangle(a,b,c)
+
+        leg_angle = max(left_leg_angle,right_leg_angle)
+        leg_ratio = left_leg_angle/right_leg_angle
+
+        # Right Upper Arm Angle
+        rigth_uarm_bend =0
+        rigth_uarm_bend = 180- findAngle(r_shldr_x, r_shldr_y, r_elbow_x, r_elbow_y)
+        
+        # Left Upper Arm Angle
+        left_uarm_bend =0
+        left_uarm_bend = 180- findAngle(r_shldr_x, r_shldr_y, r_elbow_x, r_elbow_y)
+
+        # Upper Arm Angle
+
+        uarm_bend =max(rigth_uarm_bend,left_uarm_bend)
+
+    
+        # lower Arm Angle
+        rigth_larm_bend =0
+        rigth_larm_bend = 180- findAngle(r_elbow_x, r_elbow_y, r_wrist_x, r_wrist_y)
+
+        # Wrist Angle
+        wrist_bend_angle =0
+        wrist_bend_angle =180- findAngle(r_index_x, r_index_y, r_wrist_x, r_wrist_y)
+
+
+       
     
     else:
         neck_inclination = 0
         neck_tilt= 1
-        left_bend =0
+        bend_angle =0
         trunk_twist =0
         leg_angle =0
         leg_ratio =0
-        rigth_uarm_bend =0
+        uarm_bend =0
         rigth_larm_bend =0
         wrist_bend_angle =0
 
-    return landmarked_image,neck_inclination,neck_tilt,left_bend,trunk_twist,leg_angle,leg_ratio,rigth_uarm_bend,rigth_larm_bend,wrist_bend_angle
+    return landmarked_image,neck_inclination,neck_tilt,bend_angle,trunk_twist,leg_angle,leg_ratio,uarm_bend,rigth_larm_bend,wrist_bend_angle
 
     
       
